4ac960292c62a4339f12ee9233894917990f27b5
Chaerul Rizky <riz.chaerul@gmail.com>
2023-03-02 22:50:56 +0700

    chore: remove https

--- Backend/WebService/Program.cs
+++ Backend/WebService/Program.cs
@@ -30,11 +30,8 @@ if (app.Environment.IsDevelopment())
 else if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();

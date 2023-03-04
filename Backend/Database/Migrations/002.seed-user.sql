ef46a389ed05a8a08ff10b464f2be2a3f3608c50
Chaerul Rizky <riz.chaerul@gmail.com>
2023-03-04 14:04:35 +0700

    chore: add seeder

---
+++ Backend/Database/Migrations/002.seed-user.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    users
+VALUES
+    (
+        'b17f02db-5a69-451f-ab09-3482c6fffdee',
+        'chaerul.rizky@email.com',
+        '$2a$12$aE8dbSRBPdJImWZKHJUrvOGpYX5bg9D3ZDxla44KJrnj1O6DQsAMu',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
